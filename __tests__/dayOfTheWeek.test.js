5b0548a589dea2288230fd01c400fae2da75129f
Josh Tillinghast <jwtill@icloud.com>
2022-02-02 10:54:11 -0800

    Add test for .getDayOfTheWeek
    Co-authored-by: Will W. <wjwat@onionslice.org>
    Co-authored-by: Will Greenberg <greenberg.will@gmail.com>

---
+++ __tests__/dayOfTheWeek.test.js
@@ -0,0 +1,14 @@
+import DayOfTheWeek from "../src/js/dayOfTheWeek.js";
+
+describe('DayOfTheWeek', () => {
+  test('should construct a new DayOfTheWeek object', () => {
+    const testDate = new Date("2022-02-02");
+    const dotw = new DayOfTheWeek("2022-02-02");
+    expect(dotw.day).toEqual(testDate);
+  });
+
+  test('.getDayOfWeek() should return string representing day of the week', () => {
+    const dotw = new DayOfTheWeek('2022-02-02');
+    expect(dotw.getDayofTheWeek()).toEqual('Wednesday');
+  });
+});
